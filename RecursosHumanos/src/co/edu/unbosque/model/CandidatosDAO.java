ccd583ed0eff78cc897235c9df29f66bea362da9
Johan Silva <117609781+FeelingMoon@users.noreply.github.com>
2023-02-10 07:27:18 -0500

    Delete RecursosHumanos directory

--- RecursosHumanos/src/co/edu/unbosque/model/CandidatosDAO.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.model;
-
-public interface CandidatosDAO {
-	public void ingresar(String nombre, String apellido, int cedula, int edad, String cargos);
-	public boolean modificar(String nombre, String apellido, int cedula, int edad, String cargos);
-	public void eliminar(int cedula);
-	public CandidatosDTO buscar(int cedula);
-	public String listar();
-}
